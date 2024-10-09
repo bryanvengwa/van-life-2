ea9585f0f5acf440fa481b283c62098e98ea752f
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-10-09 11:35:42 +0200

    nashes updates

--- app.mjs
+++ app.mjs
@@ -2,15 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import connectToDatabase from './src/config/dbConfig.mjs';
 import bookingRoutes from './src/routes/bookingRoutes.mjs';
+import Users from "./src/routes/userRoute.mjs"
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}))
 
 
 // mount routes here 
 
 app.use('/api' , bookingRoutes)
+app.use('/api/users' , Users)
 
 
 
