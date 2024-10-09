13132dd05ba5eb203f359f60f7469bcc8462085d
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-10-09 16:11:59 +0200

    commits

--- app.mjs
+++ app.mjs
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:false}))
 app.use('/api' , bookingRoutes)
 app.use('/api/users' , Users)
 app.use('/api/reviews', Reviews)
-// app.use('/api/vans', Van)
+app.use('/api/vans', Van)
 app.use('/api/payment', Payment)
 app.use('/api/host', Host)
 
